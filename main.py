43fd69b7bc93835aaff4c9b4ebc2e93efb700329
Chris Johnston <chris@thejohnstons.net>
2018-10-21 08:13:14 -0700

    Add a bunch of stuff

--- main.py
+++ main.py
@@ -3,23 +3,36 @@ from werkzeug.utils import secure_filename
 import json
 import configparser
 import os
+import base64
+import time
+import subprocess
+import random
 
 from google.cloud import storage
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 config = configparser.ConfigParser()
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ['jpg', 'wav', 'mp3', 'png', 'flac']
+           filename.rsplit('.', 1)[1].lower() in ['jpg', 'wav', 'mp3', 'png', 'flac', 'ogg']
 
 
 with open('config.ini') as cfg:
     config.read_string(cfg.read())
+
+
+@app.route('/tester')
+def tester():
+    return app.send_static_file('record.html')
+
 
 @app.route('/')
 def index():
@@ -47,6 +60,24 @@ def get_phrase(phrase: int):
         return "Out of range", 404
     except ValueError:
         return "Out of range", 404
+
+@app.route('/phrases/random', methods=['GET'])
+def get_random():
+    """
+    Returns a random phrase
+    :return:
+    """
+    try:
+        with open('phrases.json') as json_data:
+            # load the contents as a json object
+            data = json.load(json_data)
+            # get the index in the list
+            return jsonify(random.choice(data))
+    except IndexError:
+        return "Out of range", 404
+    except ValueError:
+        return "Out of range", 404
+
 
 
 @app.route('/phrases', methods=['GET'])
@@ -76,6 +107,20 @@ def upload_sound():
         temp_dir = config['Files']['tempdir']
         path = os.path.join(temp_dir, filename)
         file.save(path)
+
+        # standardize the format of the file
+        #os.execl('avconf', '-i', str(path), '-map', '0:a', '-codec:a', 'opus', '-b:a', '100k', '-vbr', 'on', 'file.opus')
+
+        # res = os.execl('/usr/bin/opusenc', '--downmix-mono', '--raw-rate', '48000', str(path), os.path.join(temp_dir, 'file.opus'))
+
+        # cmd = 'avconf -i ' + path + ' -f wav - | /usr/bin/opusenc --downmix-mono --raw-rate 48000 - ' + os.path.join(temp_dir, 'file.opus')
+        cmd = 'ffmpeg -y -i ' + path + ' -ar 48000 -ac 1 ' + os.path.join(temp_dir, 'file.flac')
+
+        # cmd = "/usr/bin/opusenc --downmix-mono --raw-rate 48000" + path + " " + os.path.join(temp_dir, 'file.opus')
+        subprocess.call(cmd, shell=True)
+
+        path = os.path.join(temp_dir, 'file.flac')
+
         # return the path for now, need to have this call the google
         # apis
 
@@ -83,28 +128,43 @@ def upload_sound():
         storage_client = storage.Client()
         # get the bucket to upload to
         b = storage_client.get_bucket('dubhacks2018speechrecordings')
-        blob = b.blob(filename)
+        # blob = b.blob(filename)
+        blob = b.blob('file.flac')
         blob.upload_from_filename(path)
 
         url = blob.public_url
 
-        bucket_uri = 'gs://dubhacks2018speechrecordings/' + filename
+        time.sleep(2)
 
+        bucket_uri = 'gs://dubhacks2018speechrecordings/' + 'file.flac'
+
+        # return bucket_uri
         speech_client = speech.SpeechClient()
         cc = types.RecognitionConfig(encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+                                     sample_rate_hertz=48000,
                                          language_code='en-US')
 
         # return bucket_uri
 
         response = speech_client.recognize(cc, audio={'uri': bucket_uri})
+
+        #with open(path, encoding = "ISO-8859-1") as aaa:
+        #    b64 = base64.b64encode(aaa.read().encode('ISO-8859-1'))
+            # return b64
+
+        #response = speech_client.recognize(cc, audio={'content': b64})
+
+
         #return jsonify(dir(response))
         #return response.MergeFromString
-        output = ""
+        # output = "output is:\n"
+        #
+        # for result in response.results:
+        #     output += result.alternatives[0].transcript + '\n'
 
-        for result in response.results:
-            output += result.alternatives[0].transcript + '\n'
-
-        return output
+        if len(response.results) > 0:
+            return response.results[0].alternatives[0].transcript
+        return "No audio."
 
         # return path
     return "error"
