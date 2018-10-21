925af316a8b7eb45b343bc09b36fa8471863cf94
Chris Johnston <chris@thejohnstons.net>
2018-10-21 01:24:37 -0700

    uploading and results from google compute is working

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Install prerequisites with:
 ```console
 python3.6 -m pip install -r requirements.txt
 ```
+
+# Config File
+
+```ini
+[Files]
+tempdir=/home/chris/Git/SpeakEasy/temp
+```
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, flash, redirect
+from werkzeug.utils import secure_filename
 import json
+import configparser
+import os
+
+from google.cloud import storage
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
 
 app = Flask(__name__)
 
+config = configparser.ConfigParser()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ['jpg', 'wav', 'mp3', 'png', 'flac']
+
+
+with open('config.ini') as cfg:
+    config.read_string(cfg.read())
+
 @app.route('/')
 def index():
+    # debugging
+    # file = config.get('Files', 'tempdir')
+    # print(file)
     return 'Test index, please ignore'
 
 
@@ -38,6 +59,55 @@ def allphrases():
     # have to do anything fancy with the contents
     with open('phrases.json') as json_data:
         return jsonify(json.load(json_data))
+
+@app.route('/sound', methods=['POST'])
+def upload_sound():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+
+    if file.filename == '':
+        flash('no selected file')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        temp_dir = config['Files']['tempdir']
+        path = os.path.join(temp_dir, filename)
+        file.save(path)
+        # return the path for now, need to have this call the google
+        # apis
+
+        # ok, now I get to interface w/ google cloud platform
+        storage_client = storage.Client()
+        # get the bucket to upload to
+        b = storage_client.get_bucket('dubhacks2018speechrecordings')
+        blob = b.blob(filename)
+        blob.upload_from_filename(path)
+
+        url = blob.public_url
+
+        bucket_uri = 'gs://dubhacks2018speechrecordings/' + filename
+
+        speech_client = speech.SpeechClient()
+        cc = types.RecognitionConfig(encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+                                         language_code='en-US')
+
+        # return bucket_uri
+
+        response = speech_client.recognize(cc, audio={'uri': bucket_uri})
+        #return jsonify(dir(response))
+        #return response.MergeFromString
+        output = ""
+
+        for result in response.results:
+            output += result.alternatives[0].transcript + '\n'
+
+        return output
+
+        # return path
+    return "error"
 
 # debugging use only
 if __name__ == '__main__':
